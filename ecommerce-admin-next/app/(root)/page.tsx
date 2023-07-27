768a517a1876286fd3f396b35e57522e120bbb40
muhaiminul-islam-bjit <muhaiminul.islam@bjitgroup.com>
2023-07-27 15:31:20 +0600

    feat: modal, create store, basic structure change

--- ecommerce-admin-next/app/(root)/page.tsx
+++ ecommerce-admin-next/app/(root)/page.tsx
@@ -1,21 +1,18 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
-  return (
-    <div className="p-4">
-      <UserButton
-        afterSignOutUrl="/"
-        userProfileMode="navigation"
-        userProfileUrl={
-          typeof window !== "undefined"
-            ? `${window.location.origin}/profile`
-            : undefined
-        }
-      />
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  return <div className="p-4">Root page</div>;
 };
 
 export default SetupPage;
